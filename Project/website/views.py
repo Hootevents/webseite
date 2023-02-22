4d76524a752356b033a342571cc21455f2f0cac2
Hootevents <115005685+Hootevents@users.noreply.github.com>
2023-02-22 15:07:11 +0100

    test

--- Project/website/views.py
+++ Project/website/views.py
@@ -13,36 +13,8 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-# wird nur eingeloggten Nutzern gezeigt
-@login_required
-# Wann immer zu der URL navigiert wird, wird die home funktion ausgeführt
 def home():
     # Hier könnte man nun einfach html zurückgeben
     # return "<h1>Test</h1>" - Auf der Webseite steht "Test"
 
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) <= 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    # In die render_template funktion tragen wir nun einfach den Namen des zu rendernden Templates ein.
     return render_template("home.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
